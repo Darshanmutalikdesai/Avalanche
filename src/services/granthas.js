49efe8b286c5de2f611d9b1d702c016849c7c59d
AK2042 <kaamogha2004@gmail.com>
2025-10-20 02:20:48 +0530

    Removed Unnecessary Folders

--- src/services/granthas.js
+++
@@ -1,9 +0,0 @@
-const API_URL = 'https://bgps.sambhasha.ksu.ac.in/api/';
-
-export const fetchgranthas = async () => {
-  const response = await fetch(API_URL + 'grantha/GetAllGranthas');
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-}; 
