2419ef0ba456e66aa690b62758e221c1a96fefad
Darshanmutalikdesai <darshan220062004@gmail.com>
2025-10-26 22:51:32 +0530

    local host removed

--- src/components/layout/userport.jsx
+++ src/components/layout/userport.jsx
@@ -17,7 +17,7 @@ export default function CosmicProfile() {
   const fetchProfile = async () => {
     try {
       const token = localStorage.getItem("token");
-      const res = await fetch("http://localhost:5000/api/users/profile", {
+      const res = await fetch("/api/users/profile", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
