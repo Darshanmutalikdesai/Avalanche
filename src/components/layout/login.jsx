fa27461cf550b4f80015ed2fd3705e07351b494f
Darshanmutalikdesai <darshan220062004@gmail.com>
2025-10-27 01:26:10 +0530

    added some thing

--- src/components/layout/login.jsx
+++ src/components/layout/login.jsx
@@ -65,7 +65,7 @@ const RegisterPage = () => {
   const registerUser = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/users/register", {
+      const response = await fetch("http://64.227.148.70/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -93,7 +93,7 @@ const RegisterPage = () => {
   const loginUser = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/users/login", {
+      const response = await fetch("http://64.227.148.70/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
