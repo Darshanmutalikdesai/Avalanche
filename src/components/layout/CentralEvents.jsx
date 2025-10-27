b18b6f184fde11944b9d9d9a80de42b047528102
AK2042 <kaamogha2004@gmail.com>
2025-10-28 00:16:09 +0530

    Set events to coming soon

--- src/components/layout/CentralEvents.jsx
+++ src/components/layout/CentralEvents.jsx
@@ -201,7 +201,14 @@ const CentralEvents = () => {
                 Close
               </button>
 
-              <Link
+              <button
+                className="px-6 py-3 bg-[#00f7ff] border-2 border-[#00f7ff] rounded-lg text-black font-bold transition-all duration-300 ease-in-out hover:bg-transparent hover:text-[#00f7ff] hover:shadow-[0_0_15px_#00f7ff]"
+                onClick={() => setSelectedEvent(null)}
+              >
+                Coming Soon...
+              </button>
+
+              {/*<Link
                 to={
                   selectedEvent.special
                     ? "/events/register-paper"
@@ -211,7 +218,7 @@ const CentralEvents = () => {
                 className="px-6 py-3 bg-[#00f7ff] border-2 border-[#00f7ff] rounded-lg text-black font-bold transition-all duration-300 ease-in-out hover:bg-transparent hover:text-[#00f7ff] hover:shadow-[0_0_15px_#00f7ff]"
               >
                 Register
-              </Link>
+              </Link>*/}
             </div>
           </div>
         </div>
